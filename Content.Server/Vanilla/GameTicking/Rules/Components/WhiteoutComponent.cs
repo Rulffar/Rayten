a032cb54477947a63fccb24cbb80694105b919be
Bamgeglo1485 <akkauntmin@gmail.com>
2025-06-08 23:27:12 +0900

    Фростейшен (#265)
    * База накинута
    
    * Чанджес
    
    * Фикс наверное хз
    
    * Проибался с ;
    
    * ёмаё
    
    * Патчес хуятчес
    
    * фикс
    
    * asdasdsd
    
    * asddfgsdf
    
    * Импрувментс
    
    * музычка
    
    * fixes
    
    * Импрувмент + руководство
    
    * Hot Fix
    
    * fasdas
    
    * fix
    
    * ура савдаст оатмеал
    
    * вот щас
    
    * аптымызацыя
    
    * ывывыфв
    
    * Шкафчики
    
    * чанджес хз какие
    
    * Подготовка к ФИНАЛ ОЧКЕ
    
    * чандапдываывавва
    
    * аываыва
    
    * импрувы тип
    
    * optimizacion
    
    * finalo4ka
    
    * Фиксы
    
    * Обновление
    
    * ОБНОВКА
    
    * final fixes
    
    ---------
    
    Co-authored-by: TFMB <mail@vanilla-station.ru>

---
+++ Content.Server/Vanilla/GameTicking/Rules/Components/WhiteoutComponent.cs
@@ -0,0 +1,91 @@
+using Content.Server.GameTicking.Rules.Components;
+using Robust.Shared.Audio;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom;
+using Robust.Shared.Map;
+using Content.Shared.Weather;
+
+namespace Content.Server.Vanilla.GameTicking.Rules.WhiteOut;
+
+[RegisterComponent, Access(typeof(WhiteoutRuleSystem))]
+public sealed partial class WhiteoutRuleComponent : Component
+{
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public bool PlanetMap = false;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutLength = 900;
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutFinalLength = 300f;
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutPrepareTime = 3000f;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutPrepareTemp = 253.15f;
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutTemp = 123.15f;
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutFinalTemp = 23.15f;
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutPrepareStrength = 0.005f;
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutStrength = 0.05f;
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public float WhiteoutFinalModifier = 1.5f;
+
+    [DataField]
+    public string Weather = "SnowfallHeavy";
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public LocId WhiteoutPrepareAnnouncement = "whiteout-prepare-announcement";
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public LocId WhiteoutPrestartAnnouncement = "whiteout-prestart-announcement";
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public LocId WhiteoutAnnouncement = "whiteout-announcement";
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public LocId WhiteoutFinalAnnouncement = "whiteout-announcement-final";
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public LocId WhiteoutEndAnnouncement = "whiteout-announcement-end";
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public SoundSpecifier WhiteoutSoundAnnouncement = new SoundPathSpecifier("/Audio/Vanilla/StationEvents/announcement.ogg");
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public SoundSpecifier WhiteoutAlarmSound = new SoundPathSpecifier("/Audio/Vanilla/StationEvents/whiteout_siren.ogg");
+
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public SoundSpecifier WhiteoutMusic = new SoundPathSpecifier("/Audio/Vanilla/StationEvents/whiteout.ogg");
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public SoundSpecifier WhiteoutPrestartMusic = new SoundPathSpecifier("/Audio/Vanilla/StationEvents/whiteout_prestart.ogg");
+    [DataField, ViewVariables(VVAccess.ReadWrite)]
+    public SoundSpecifier WhiteoutFinalMusic = new SoundPathSpecifier("/Audio/Vanilla/StationEvents/whiteout_final.ogg");
+
+    public float TimeActive;
+    public TimeSpan NextUpdate;
+    public WhiteoutState CurrentState = WhiteoutState.Ended;
+    public MapId ActiveMapId = MapId.Nullspace;
+    public EntityUid ActiveMapUid = EntityUid.Invalid;
+    public TimeSpan NextGlassBreak;
+    public bool PrestartPlayed = false;
+
+    /// <summary>
+    /// Вычисляет что-то я хз
+    /// </summary>
+    public (float Temp, float Strength) GetWhiteoutParams(bool isFinal)
+    {
+        var duration = WhiteoutLength + WhiteoutFinalLength;
+        var strengthFactor = WhiteoutStrength * (TimeActive / duration);
+
+        if (isFinal)
+            return (WhiteoutFinalTemp, strengthFactor * WhiteoutFinalModifier);
+        else
+            return (WhiteoutTemp, strengthFactor);
+    }
+}
+public enum WhiteoutState : byte
+{
+    Preparing = 0,
+    Active = 1,
+    FinalPhase = 3,
+    Ended = 4,
+}
---
+++ Content.Server/Vanilla/GameTicking/Rules/WhiteoutRuleSystem.cs
@@ -0,0 +1,422 @@
+using Content.Server.GameTicking.Rules.Components;
+using Content.Server.Administration.Managers;
+using Content.Server.Explosion.EntitySystems;
+using Content.Server.Temperature.Components;
+using Content.Server.Atmos.EntitySystems;
+using Content.Server.Light.EntitySystems;
+using Content.Server.GameTicking.Rules;
+using Content.Server.Tesla.Components;
+using Content.Server.Cargo.Components;
+using Content.Server.Atmos.Components;
+using Content.Server.Light.Components;
+using Content.Server.Chat.Managers;
+using Content.Server.Chat.Systems;
+using Content.Server.Power.SMES;
+using Content.Server.RoundEnd;
+using Content.Server.Weather;
+using Content.Server.Damage;
+using Content.Server.Resist;
+using Content.Server.Audio;
+
+using Content.Shared.GameTicking.Components;
+using Content.Shared.FixedPoint;
+using Content.Shared.Weather;
+using Content.Shared.Damage;
+using Content.Shared.Atmos;
+using Content.Shared.Audio;
+using Content.Shared.Maps;
+using Content.Shared.Tag;
+
+using Robust.Shared.Map.Components;
+using Robust.Shared.Configuration;
+using Robust.Shared.Audio.Systems;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
+using Robust.Shared.Random;
+using Robust.Shared.Player;
+using Robust.Shared.Audio;
+using Robust.Shared.Map;
+
+using Robust.Server.Player;
+
+using System.Linq;
+
+namespace Content.Server.Vanilla.GameTicking.Rules.WhiteOut;
+
+public sealed class WhiteoutRuleSystem : GameRuleSystem<WhiteoutRuleComponent>
+{
+    [Dependency] private readonly IPrototypeManager _prototypeManager = default!;
+    [Dependency] private readonly PoweredLightSystem _poweredLight = default!;
+    [Dependency] private readonly AtmosphereSystem _atmosphere = default!;
+    [Dependency] private readonly DamageableSystem _damageable = default!;
+    [Dependency] private readonly SharedAudioSystem _audio = default!;
+    [Dependency] private readonly RoundEndSystem _roundEnd = default!;
+    [Dependency] private readonly IGameTiming _gameTiming = default!;
+    [Dependency] private readonly IMapManager _mapManager = default!;
+    [Dependency] private readonly WeatherSystem _weather = default!;
+    [Dependency] private readonly ExplosionSystem _boom = default!;
+    [Dependency] private readonly TagSystem _tagSystem = default!;
+    [Dependency] private readonly ChatSystem _chat = default!;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    // Базовые действия при начале геймрула
+    protected override void Started(EntityUid uid, WhiteoutRuleComponent comp, GameRuleComponent rule, GameRuleStartedEvent args)
+    {
+        base.Started(uid, comp, rule, args);
+
+        comp.TimeActive = 0f;
+        comp.NextUpdate = _gameTiming.CurTime + TimeSpan.FromSeconds(1);
+
+        comp.CurrentState = WhiteoutState.Preparing;
+
+        comp.NextGlassBreak = TimeSpan.Zero;
+
+        comp.PrestartPlayed = false;
+
+        if (comp.ActiveMapId == MapId.Nullspace)
+        {
+            var xform = Transform(uid);
+            comp.ActiveMapId = xform.MapID;
+            if (comp.ActiveMapId == MapId.Nullspace)
+            {
+                comp.ActiveMapId = _mapManager.GetAllMapIds().FirstOrDefault(id => id != MapId.Nullspace);
+                if (comp.ActiveMapId == MapId.Nullspace)
+                    return;
+            }
+            comp.ActiveMapUid = _mapManager.GetMapEntityId(comp.ActiveMapId);
+        }
+
+        // Удаление компонента трейдпоста у поста для усложнения
+        var query = EntityQueryEnumerator<TradeStationComponent>();
+        while (query.MoveNext(out var tradepost, out _))
+        {
+            RemComp<TradeStationComponent>(tradepost);
+        }
+
+        _chat.DispatchGlobalAnnouncement(Loc.GetString(comp.WhiteoutPrepareAnnouncement), playSound: true, announcementSound: comp.WhiteoutSoundAnnouncement, colorOverride: Color.Red);
+    }
+
+    public override void Update(float frameTime)
+    {
+        base.Update(frameTime);
+        var currentTime = _gameTiming.CurTime;
+        var query = EntityQueryEnumerator<WhiteoutRuleComponent, GameRuleComponent>();
+
+        while (query.MoveNext(out var uid, out var comp, out var rule))
+        {
+            if (currentTime < comp.NextUpdate) continue;
+
+            comp.NextUpdate = currentTime + TimeSpan.FromSeconds(1);
+            comp.TimeActive += 1f;
+            ProcessWhiteout(uid, comp, rule);
+        }
+    }
+
+    // Сам вайтаут
+    private void ProcessWhiteout(EntityUid uid, WhiteoutRuleComponent comp, GameRuleComponent rule)
+    {
+        var now = _gameTiming.CurTime;
+        switch (comp.CurrentState)
+        {
+            case WhiteoutState.Preparing:
+
+                if (comp.TimeActive >= comp.WhiteoutPrepareTime)
+                {
+                    StartWhiteout(uid, comp, comp.ActiveMapId);
+                    comp.CurrentState = WhiteoutState.Active;
+                }
+                else
+                {
+                    var isPhaseTwo = comp.TimeActive >= comp.WhiteoutPrepareTime / 2;
+                    var prepareTemp = isPhaseTwo ? comp.WhiteoutPrepareTemp * 2f : comp.WhiteoutPrepareTemp;
+                    var prepareStrength = isPhaseTwo ? comp.WhiteoutPrepareStrength * 2f : comp.WhiteoutPrepareStrength;
+
+                    Freeze(prepareTemp, prepareStrength, comp.ActiveMapId);
+
+                    if (comp.TimeActive >= comp.WhiteoutPrepareTime - 240f & !comp.PrestartPlayed)
+                    {
+                        comp.PrestartPlayed = true;
+                        _audio.PlayGlobal(comp.WhiteoutPrestartMusic, Filter.Broadcast(), true);
+                        _chat.DispatchGlobalAnnouncement(Loc.GetString(comp.WhiteoutPrestartAnnouncement), colorOverride: Color.Red);
+                    }
+                }
+
+                break;
+
+            case WhiteoutState.Active:
+            case WhiteoutState.FinalPhase:
+                var isFinal = comp.CurrentState == WhiteoutState.FinalPhase;
+                var (temp, strength) = comp.GetWhiteoutParams(isFinal);
+                Freeze(temp, strength, comp.ActiveMapId);
+                ExplodeSmes(comp.ActiveMapUid, comp.ActiveMapId);
+                ChangeTiles(comp.ActiveMapId);
+
+                // Повреждение объектов
+                if (now >= comp.NextGlassBreak)
+                {
+                    var damage = new DamageSpecifier();
+                    damage.DamageDict.Add("Blunt", FixedPoint2.New(130));
+                    //лампы
+                    var lampQuery = EntityQueryEnumerator<PoweredLightComponent, TransformComponent>();
+                    while (lampQuery.MoveNext(out var lampEnt, out var light, out var xform))
+                    {
+                        if (CheckTileTemperature(lampEnt, 183.15f) && RobustRandom.Prob(0.3f) && xform.MapID == comp.ActiveMapId)
+                        {
+                            _poweredLight.TryDestroyBulb(lampEnt, light);
+                        }
+                    }
+                    //шкафы
+                    var lockerQuery = EntityQueryEnumerator<ResistLockerComponent, TransformComponent>();
+                    while (lockerQuery.MoveNext(out var lockerEnt, out _, out var xform))
+                    {
+                        if (CheckTileTemperature(lockerEnt, 133.15f) && RobustRandom.Prob(0.5f) && xform.MapID == comp.ActiveMapId)
+                        {
+                            _damageable.TryChangeDamage(lockerEnt, damage);
+                        }
+                    }
+                    //окна
+                    if (isFinal)
+                    {
+                        var windowQuery = EntityQueryEnumerator<TransformComponent>();
+                        while (windowQuery.MoveNext(out var windowEnt, out var xform))
+                        {
+                            if (_tagSystem.HasTag(windowEnt, "Window") &&
+                                xform.MapID == comp.ActiveMapId &&
+                                RobustRandom.Prob(0.8f))
+                            {
+                               _damageable.TryChangeDamage(windowEnt, damage);
+                            }
+                        }
+                    }
+
+                    comp.NextGlassBreak = now + TimeSpan.FromSeconds(5);
+                }
+
+                var totalDuration = comp.WhiteoutLength + comp.WhiteoutFinalLength;
+                var finalPhaseEndWarning = totalDuration - 60f;
+
+                if (!isFinal)
+                {
+                    if (comp.TimeActive >= comp.WhiteoutLength)
+                    {
+                        GameTicker.AddGameRule("GameRuleMeteorSwarmLarge");
+                        MakeAtmos(comp.WhiteoutFinalTemp, comp.PlanetMap, comp.ActiveMapId, comp.ActiveMapUid);
+                        comp.CurrentState = WhiteoutState.FinalPhase;
+                        _audio.PlayGlobal(comp.WhiteoutFinalMusic, Filter.Broadcast(), true);
+                        _audio.PlayGlobal(comp.WhiteoutAlarmSound, Filter.Broadcast(), true);
+                        _chat.DispatchGlobalAnnouncement(Loc.GetString(comp.WhiteoutFinalAnnouncement), colorOverride: Color.Red);
+                    }
+                }
+                else
+                {
+                    if (comp.TimeActive >= finalPhaseEndWarning)
+                    {
+                        _roundEnd.RequestRoundEnd(TimeSpan.FromMinutes(1), uid, false, "whiteout-evac", "department-CentralCommand");
+                    }
+
+                    if (comp.TimeActive >= totalDuration)
+                    {
+                        EndWhiteout(uid, comp, rule, comp.ActiveMapId);
+                        comp.CurrentState = WhiteoutState.Ended;
+                    }
+                }
+                break;
+        }
+    }
+
+
+    // Действия при начале
+    private void StartWhiteout(EntityUid uid, WhiteoutRuleComponent comp, MapId mapId)
+    {
+        comp.TimeActive = 0f;
+
+        MakeAtmos(comp.WhiteoutTemp, comp.PlanetMap, comp.ActiveMapId, comp.ActiveMapUid);
+
+        GameTicker.AddGameRule("MeteorSwarmWhiteoutScheduler");
+        GameTicker.AddGameRule("GameRuleMeteorSwarmLarge");
+
+        _audio.PlayGlobal(comp.WhiteoutMusic, Filter.Broadcast(), true);
+
+        if (!_prototypeManager.TryIndex<WeatherPrototype>(comp.Weather, out var weatherProto))
+            return;
+
+        _weather.SetWeather(mapId, weatherProto, TimeSpan.FromMinutes(30));
+
+        RemoveHardsuitProtection();
+
+        _chat.DispatchGlobalAnnouncement(Loc.GetString(comp.WhiteoutAnnouncement), colorOverride: Color.Red);
+        _audio.PlayGlobal(comp.WhiteoutAlarmSound, Filter.Broadcast(), true);
+    }
+
+    // Действия при конце
+    private void EndWhiteout(EntityUid uid, WhiteoutRuleComponent comp, GameRuleComponent rule, MapId mapId)
+    {
+        GameTicker.EndGameRule(uid, rule);
+        _weather.SetWeather(mapId, null, TimeSpan.FromMinutes(1));
+
+        RemComp<MapAtmosphereComponent>(comp.ActiveMapUid);
+
+        AddHardsuitProtection();
+        _chat.DispatchGlobalAnnouncement(Loc.GetString(comp.WhiteoutEndAnnouncement), playSound: true, announcementSound: comp.WhiteoutSoundAnnouncement, colorOverride: Color.Red);
+    }
+
+    // Заморозка газов
+    private void Freeze(float targetTemp, float strength, MapId mapId)
+    {
+        var query = EntityQueryEnumerator<GridAtmosphereComponent, TransformComponent>();
+        while (query.MoveNext(out var gridUid, out var grid, out var gridXform))
+        {
+            if (gridXform.MapID != mapId)
+                continue;
+
+            foreach (var tile in grid.Tiles.Values)
+            {
+                if (tile.Air is not { Pressure: >= 1f, TotalMoles: > 0f } mixture)
+                    continue;
+
+                var heatCap = _atmosphere.GetHeatCapacity(mixture, false);
+                if (heatCap <= 0)
+                    continue;
+
+                var temp = mixture.Temperature;
+                if (temp <= targetTemp)
+                    continue;
+
+                float delta = (temp - targetTemp) * strength;
+                mixture.Temperature = Math.Max(targetTemp, temp - delta);
+            }
+        }
+    }
+
+    // Убирание или добавление резистов скафов
+    private void AddHardsuitProtection()
+    {
+        var query = EntityQueryEnumerator<TagComponent>();
+        while (query.MoveNext(out var uid, out _))
+        {
+            if (_tagSystem.HasTag(uid, "Hardsuit"))
+            {
+                AddComp<TemperatureProtectionComponent>(uid);
+                AddComp<PressureProtectionComponent>(uid);
+            }
+        }
+    }
+    private void RemoveHardsuitProtection()
+    {
+        var query = EntityQueryEnumerator<TemperatureProtectionComponent, PressureProtectionComponent, TransformComponent>();
+        while (query.MoveNext(out var uid, out _, out _, out _))
+        {
+            if (_tagSystem.HasTag(uid, "Hardsuit"))
+            {
+                RemComp<TemperatureProtectionComponent>(uid);
+                RemComp<PressureProtectionComponent>(uid);
+            }
+        }
+    }
+
+    // Взрыв смэсов
+    private void ExplodeSmes(EntityUid ActiveMapUid, MapId ActiveMapId)
+    {
+        if (!Exists(ActiveMapUid))
+            return;
+
+        var query = EntityQueryEnumerator<SmesComponent, TransformComponent>();
+        while (query.MoveNext(out var uid, out _, out var xform))
+        {
+            if (xform.MapID == ActiveMapId && CheckTileTemperature(uid, 133.15f))
+                _boom.QueueExplosion( uid, "Cryo", 200, 10, 200 );
+        }
+    }
+
+    // Проверка температуры тайла энтити, для проведения действий
+    private bool CheckTileTemperature(EntityUid uid, float threshold)
+    {
+        if (!Exists(uid) || Deleted(uid))
+            return false;
+
+        if (!TryComp<TransformComponent>(uid, out var transform) || transform.GridUid == null)
+            return false;
+
+        if (!TryComp<MapGridComponent>(transform.GridUid, out var grid))
+            return false;
+
+        var tile = grid.TileIndicesFor(transform.Coordinates);
+        var atmosphere = _atmosphere.GetTileMixture(transform.GridUid, null, tile, true);
+
+        return atmosphere?.Temperature < threshold;
+    }
+
+    // Смена тайлов на снежочек
+    private void ChangeTiles(MapId ActiveMapId)
+    {
+        float temp = 122f;
+
+        if (!_prototypeManager.TryIndex<ContentTileDefinition>("FloorSnowPlating", out var iceTile))
+            return;
+
+        var query = EntityQueryEnumerator<GridAtmosphereComponent, MapGridComponent>();
+        while (query.MoveNext(out var gridUid, out var gridAtmos, out var grid))
+        {
+            if (Transform(gridUid).MapID != ActiveMapId)
+                continue;
+
+            foreach (var (tile, atmosTile) in gridAtmos.Tiles)
+            {
+
+                if (atmosTile.Air == null)
+                    continue;
+
+                bool shouldChange = atmosTile.Air.Temperature < temp || atmosTile.Air.GetMoles(Gas.Frezon) > 0f;
+
+                if (!shouldChange)
+                    continue;
+
+                var tileRef = grid.GetTileRef(tile);
+                var tileDef = tileRef.Tile.GetContentTileDefinition();
+
+                if (tileRef.Tile.IsEmpty ||
+                    tileDef.ID == "FloorSnowPlating" ||
+                    tileDef.ID == "Lattice")
+                    continue;
+
+                if (RobustRandom.Prob(0.3f))
+                {
+                    grid.SetTile(tile, new Tile(iceTile.TileId));
+                }
+            }
+        }
+    }
+
+    // Создание атмосферы при начале бури, реалистик и +сложность
+    private void MakeAtmos(float temp, bool planet, MapId ActiveMapId, EntityUid ActiveMapUid)
+    {
+        if (!_mapManager.MapExists(ActiveMapId))
+            return;
+
+        var moles = new float[Atmospherics.AdjustedNumberOfGases];
+
+        if (planet)
+        {
+            moles[(int)Gas.Oxygen] = 43.649558f;
+            moles[(int)Gas.Nitrogen] = 164.20624f;
+        }
+        else
+        {
+            moles[(int)Gas.Oxygen] = 126f;
+            moles[(int)Gas.Frezon] = 141f;
+            moles[(int)Gas.NitrousOxide] = 132f;
+        }
+
+        var mixture = new GasMixture(moles, temp);
+
+        if (Exists(ActiveMapUid))
+        {
+            var mapAtmos = EnsureComp<MapAtmosphereComponent>(ActiveMapUid);
+            _atmosphere.SetMapAtmosphere(ActiveMapUid, false, mixture);
+        }
+    }
+}
